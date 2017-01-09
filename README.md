388dcb80c798b89bb6b7338eb865b28e096a449b
Taka <yyyymmddhhmmsssss@gmail.com>
2017-01-09 15:52:05 +0900

    Init

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# csharpZipInvestigation
-.Net Zip Class Investigation
+# Zip圧縮ツール
+
+.NET4.0から追加されたZip機能の調査ツールの作成のためのツール
+
+## Specification
+
+.NET Console Application (.NET 4.6.2)
+
+**CommandLine**
+
+> ReZipper.exe $SourceDir $DestDir
+
+**Args**
+* $SourceDir
+  * 圧縮対象のフォルダ
+
+
+* $DestDir
+  * 圧縮ファイルの出力先のフォルダ
+
+## Exsample
+
+ReZipper.exe d:\source d:\dest
+
+* **Input**
+  * d:\source\
+    * Dir1\
+      * File101.jpg
+      * File102.jpg
+    * Dir2\
+      * File201.jpg
+      * File202.jpg
+
+
+* **Output**
+  * d:\dest
+    * **Dir1.zip\\**
+      * File101.jpg
+      * File102.jpg
+    * **Dir2.zip\\**
+      * File201.jpg
+      * File202.jpg
+
+
+> **Note:**
+>
+> zip compression level is not compressed.
---
+++ Reziper/AppMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReZipper
+{
+    public class AppMain
+    {
+        internal static void Main(string[] args)
+        {
+            checkArgs(args);
+
+            foreach (var dir in Directory.GetDirectories(args[0]))
+            {
+                string dirName = Path.GetFileName(dir);
+                string zipName = dirName + ".zip";
+                string zipPath = Path.Combine(args[1], zipName);
+
+                Console.WriteLine("[" + dirName + "]");
+                using (var zip = ZipFile.Open(zipPath, ZipArchiveMode.Create, Encoding.UTF8))
+                {
+                    foreach (var filePath in Directory.GetFiles(dir))
+                    {
+                        zip.CreateEntryFromFile(filePath, Path.GetFileName(filePath), CompressionLevel.NoCompression);
+                        Console.WriteLine("  /File : " + Path.GetFileName(filePath));
+                    }
+                }
+            }
+
+            Console.WriteLine("Completed.");
+            //Console.ReadLine();
+        }
+
+        private static bool checkArgs(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalied args.");
+                return false;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("Source dir is not found.");
+                return false;
+            }
+
+            if (!Directory.Exists(args[1]))
+            {
+                Console.WriteLine("Destination dir is not found.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
